6fbd98e01c4b0b429823984d3829fc2bd7035fac
Benjamin Minato <60768739+Top-Dev-Ops@users.noreply.github.com>
2021-06-13 23:50:08 +0800

    disable redux dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -17,7 +17,7 @@ const store = createStore(
     initialState,
     compose(
         applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 );
 
